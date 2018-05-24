fa83f561945d39fc8fe14034bbf292c04092f46c
Mohammad Aziz <mdaziz067@gmail.com>
2018-05-24 22:40:24 +0530

    First version

---
+++ README.md
@@ -0,0 +1,5 @@
+# JSX to JS converter
+
+This converts every file with `.jsx` extension to `.js`. It start from the folder where you run the script.
+
+**NOTE**: This doesn't fix the relative path within the files. That you have to do by yourself.
---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+function renameIt(oldName, newName) {
+  fs.rename(oldName, newName, (err) => {
+    if (err) throw err;
+    console.log('done', oldName);
+  })
+}
+
+function crawlFiles(startingPoint, excludeFolder) {
+  fs.readdir(startingPoint, (err, files) => {
+    if (err) throw err;
+    if (!files.length) {
+      return;      
+    } else {
+      files.forEach((file) => {
+        let isDir, isFile;
+        const stats = fs.lstatSync(`${startingPoint}/${file}`);
+        isDir = stats.isDirectory();
+        isFile = stats.isFile();
+        if (isFile && path.extname(file) === '.jsx') {
+          let newFileName = file.substring(0, file.lastIndexOf('.')) + '.js';
+          renameIt(`${startingPoint}/${file}`, `${startingPoint}/${newFileName}`);
+        } else if (isDir && file !== excludeFolder) {
+          crawlFiles(`${startingPoint}/${file}`)
+        }
+      })
+    }
+  })
+}
+
+crawlFiles(__dirname, 'node_modules');
